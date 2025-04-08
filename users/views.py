27897b90f4669ebae77014edd2bb18c2489cf08d
Jean de Dieu UWINTWALI <uwintwalijeandedieu3@gmail.com>
2025-04-08 11:18:05 +0200

    successfully fixed selection dropdown for location

--- users/views.py
+++ users/views.py
@@ -38,7 +38,30 @@ def user_delete(request, pk):
 def get_locations(request, location_type):
     parent_id = request.GET.get('parent')
     locations = Location.objects.filter(type=location_type.upper())
+    
     if parent_id:
-        locations = locations.filter(location_id__startswith=parent_id)  # Filtering logic
+        # Convert parent_id to integer
+        parent_id = int(parent_id)
+        
+        # Filter based on location hierarchy
+        if location_type.upper() == 'PROVINCE':
+            # Provinces have IDs 1-5
+            locations = locations.filter(location_id__gte=1, location_id__lte=5)
+        elif location_type.upper() == 'DISTRICT':
+            # Districts start with the province number
+            province_prefix = str(parent_id)
+            locations = locations.filter(location_id__gte=int(province_prefix + '1'), 
+                                      location_id__lte=int(province_prefix + '9'))
+        elif location_type.upper() == 'SECTOR':
+            # Sectors start with the district ID
+            district_prefix = str(parent_id)
+            locations = locations.filter(location_id__gte=int(district_prefix + '01'), 
+                                      location_id__lte=int(district_prefix + '99'))
+        elif location_type.upper() == 'CELL':
+            # Cells start with the sector ID
+            sector_prefix = str(parent_id)
+            locations = locations.filter(location_id__gte=int(sector_prefix + '01'), 
+                                      location_id__lte=int(sector_prefix + '99'))
+    
     data = [{'id': loc.location_id, 'name': loc.name} for loc in locations]
     return JsonResponse(data, safe=False)
